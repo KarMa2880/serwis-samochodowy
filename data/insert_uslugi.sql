105d0af479867e933e345e97b1947a4fcf2d4000
KarMa2880 <marckarm2880@gmail.com>
2026-01-07 18:21:41 +0100

    Fix duplicate entry for 'Wymiana opon' in SQL

--- data/insert_uslugi.sql
+++ data/insert_uslugi.sql
@@ -5,7 +5,7 @@ VALUES
     ('Wymiana klocków hamulcowych', 300.00),
     ('Diagnostyka komputerowa', 120.00),
     ('Przegląd okresowy', 250.00),
-    ('Wymiana opon', 200.00)
+    ('Wymiana opon', 200.00),
     ('Wymiana rozrządu', 500.00),
     ('Wymiana zawieszenia', 400.00),
     ('Serwis skrzyni biegów', 600.00),
